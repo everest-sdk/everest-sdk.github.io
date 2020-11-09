f6da04c8556d95695ac591fb5716302d552f9eb1
Brayan Martinez <bmartinez@intelix.biz>
2020-11-08 20:47:45 -0400

    gh-pages

---
+++ precache-manifest.9d33a40b9bb299ef481b55652aeca645.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dc6338ecfb5534d53c",
+    "url": "/css/chunk-vendors.5a8c56b9.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7cea72af2081deea75a4439993d9f605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e0bba970a9d98db7ae4",
+    "url": "/js/about.e81f9217.js"
+  },
+  {
+    "revision": "c6328c02bf80b4bbf75b",
+    "url": "/js/app.e547456c.js"
+  },
+  {
+    "revision": "20dc6338ecfb5534d53c",
+    "url": "/js/chunk-vendors.eacaf074.js"
+  },
+  {
+    "revision": "d8798ac073623af54b916af8ff6c2219",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
